3e42f8156a46280f722079237953e313eb9c4111
Björn Ahlers <rewired.de@gmail.com>
2026-02-13 01:09:30 +0100

    Fix client-web App type portability for CI build

--- packages/client-web/src/App.tsx
+++ packages/client-web/src/App.tsx
@@ -1,10 +1,11 @@
 import { Client } from 'boardgame.io/react';
+import type { ComponentType } from 'react';
 import * as GameModule from '@balance-control/game';
 import { Board } from './Board';
 
 const BalanceControl = (GameModule as any).BalanceControl;
 
-const App = Client({
+const App: ComponentType = Client({
     game: BalanceControl,
     board: Board,
 });
